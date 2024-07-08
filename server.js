573c3894f5dbe028e6d46e066c4e3b4702c1cc84
samarhirau <samrhirau@gmail.com>
2024-07-08 23:09:31 +0530

    folders

--- server.js
+++ server.js
@@ -19,99 +19,6 @@ app.use(express.urlencoded({extended:true}))
 
 
 
-
-
-// mongoose.connect('mongodb://127.0.0.1:27017/students')
-// const db_ = mongoose.connection
-// db_.once('open',()=>{
-//     console.log("Mongodb connection successful")
-// })
-
-// const userSchema = new mongoose.Schema({
-   
-//   firstname: {
-//     type: String,
-//     required: true
-// },
-// lastname: {
-//     type: String,
-//     required: true
-// },
-// email: {
-//     type: String,
-//     required: true,
-//     unique: true
-// },
-// password: {
-//     type: String,
-//     required: true
-// },
-// phone: {
-//     type: String,
-//     required: true
-// },
-// dob: {
-//     type: Date,
-//     required: true
-// },
-// gender: {
-//     type: String,
-//     // enum: ['Male', 'Female', 'Other'],
-//     required: true
-// },
-// address: {
-//     type: String,
-//     required: true
-// },
-// course: {
-//     type: String,
-//     required: true
-// }
-    
-// })
-
-// const Users = mongoose.model("data",userSchema)
-
-// // Serve static files from the 'public' directory
-// // app.use(express.static(path.join(__dirname, 'public')));
-// app.get('/form',(req, res) => {
-//     res.sendFile(path.join(__dirname, 'public','form.html'))
-//     // res.send('Hello World')
-// })
-
-// app.post('/post', async(req,res)=>{
-
-//   try {
-//     // Assuming you have a 'Users' model defined
-
-//     const hashedPassword = crypto.createHash('sha256').update(req.body.password).digest('hex');
-//     const user = new Users({
-//         firstname: req.body.firstname,
-//         lastname: req.body.lastname,
-//         email: req.body.email,
-//         // password: req.body.password,
-//         password: hashedPassword,
-//         phone: req.body.phone,
-//         dob: req.body.dob,
-//         gender: req.body.gender,
-//         address: req.body.address,
-//         course: req.body.course
-//     });
-//     await user.save();
-//     res.send("Data saved");
-//     console.log("User registered:", req.body);
-// } catch (error) {
-//     res.status(400).send('Email is already exist'); // Sending error message back to the client
-//     console.error("Error:", error);
-// }
-
-// })
-
-// MongoDB connection
-// mongoose.connect('mongodb://127.0.0.1:27017/students', {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// });
 mongoose.connect('mongodb://127.0.0.1:27017/students');
 
 const db_ = mongoose.connection;
@@ -257,30 +164,6 @@ app.get('/admin', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'admin.html'));
 });
 
-// Route to handle form submission
-// app.post('/upload', upload.single('certificate'), async (req, res) => {
-//   const { mobileNumber } = req.body;
-//   const certificateFile = req.file;
-
-//   if (!mobileNumber || !certificateFile) {
-//     return res.status(400).send('Mobile number and certificate file are required');
-//   }
-
-//   try {
-//     const certificate = {
-//       mobileNumber,
-//       certificatePath: certificateFile.path,
-//       createdAt: new Date()
-//     };
-
-//     await db.collection('certificates').insertOne(certificate);
-    
-//     res.sendFile(path.join(__dirname, 'public', 'uploaded.html'));
-//     // res.status(200).send('Certificate uploaded successfully');
-//   } catch (err) {
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 // Route to handle form submission
 app.post('/upload', upload.single('certificate'), async (req, res) => {
 
