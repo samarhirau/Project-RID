85b1a1b2861542b5223db397d5ef41cb45f6014b
samarhirau <samrhirau@gmail.com>
2024-08-14 12:22:51 +0530

    clean the code folder structure

--- routes/verify.js
+++ routes/verify.js
@@ -1,16 +1,4 @@
 
-// const express = require('express');
-// const router = express.Router();
-// const { verifyCertificate, getCertificateFile } = require('../controllers/verifyController');
-
-// // Route to handle certificate verification
-// router.post('/verify', verifyCertificate);
-
-// // Route to serve certificate files
-// router.get('/certificate/:id', getCertificateFile);
-
-// module.exports = router;
-
 
 const express = require('express');
 const router = express.Router();
